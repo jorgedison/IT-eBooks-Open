53abc01a960e1e479dee17b4a499d24f1ea84860
Jorge Rodriguez <jrodriguezf@outlook.com>
2014-08-21 02:59:08 -0500

    modified function error

--- protected/exceptions/exceptions.php
+++ protected/exceptions/exceptions.php
@@ -3,6 +3,7 @@
 class exceptions{
    
     function exceptions($code){
+
         switch ($code) {
             case 404:
                 $result['errors'] = array("Code"=>"","message"=>"Page not found");
@@ -10,6 +11,7 @@ class exceptions{
         default:
             $result['errors'] = array("Code"=>"", "It has happened a fatal error");
         }
+
         $json_string = json_encode($result, JSON_PRETTY_PRINT);
         return $json_string;
     }
