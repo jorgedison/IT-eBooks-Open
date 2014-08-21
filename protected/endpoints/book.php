97652dc895bb39431bb5e2b5137227652c37169f
Jorge Rodriguez <jrodriguezf@outlook.com>
2014-08-21 02:58:36 -0500

    add function book details

--- protected/endpoints/book.php
+++ protected/endpoints/book.php
@@ -2,12 +2,11 @@
 
 class book{
 
-    function getBook(){
-    
-    }
-
-    function getBookId(){
-
+    function getBookId($id){
+        $url = ApiToolkit::getHomeUrl().'book/'.$id;
+        $json = file_get_contents($url,0,null,null);
+        $json_output = json_decode($json, true);
+        return var_dump(array($json_output));
     }
 
 }
