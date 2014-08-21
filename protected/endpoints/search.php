1233ad3daa5f8062066d0d7e86a5d044e28727ec
Jorge Rodriguez <jrodriguezf@outlook.com>
2014-08-21 02:41:32 -0500

    modified function search

--- protected/endpoints/search.php
+++ protected/endpoints/search.php
@@ -6,13 +6,14 @@ class search{
         $url = ApiToolkit::getHomeUrl().'search/'.$query;
         $json = file_get_contents($url,0,null,null);        
         $json_output = json_decode($json, true);
-        #return array["Books"];
-        return var_dump(array($json_output["Books"]));
+        return var_dump(array($json_output));
     }
 
     function getSearchPage($query, $number){
-        $url = ApiToolkit::getHomeUrl();
-        return $url;
+        $url = ApiToolkit::getHomeUrl().'search/'.$query.'/page/'.$number;
+        $json = file_get_contents($url,0,null,null);        
+        $json_output = json_decode($json, true);
+        return var_dump(array($json_output));
     }
 }
 
